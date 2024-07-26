8ea73cb67625a1c84ddf7b531bd250bec2bfd5fa
cemre <cemresumerr8@gmail.com>
2024-07-26 17:56:32 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_signupBtn_Click(object sender, EventArgs e)
+        {
+            RegisterForm regForm = new RegisterForm();
+            regForm.Show();
+            this.Hide();
+        }
+
+        private void login_password_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            login_password.PasswordChar = login_showPassword.Checked ? '\0' : '*';
+        }
+    }
+}
---
+++ RegisterForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace EmployeeManagement
+{
+    public partial class RegisterForm : Form
+    {
+
+        SqlConnection connect
+            = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\CEMRESU\Documents\employee.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void signup_loginBtn_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void signup_btn_Click(object sender, EventArgs e)
+        {
+            if(signup_username.Text == ""
+                || signup_password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields"
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+                if(connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+
+                        string selectUsername = "SELECT * FROM users WHERE username = @username";
+
+
+                        DateTime today = DateTime.Today;
+
+                        string insertData = "INSERT INTO users " +
+                            "(username, password, date_register )" +
+                            " VALUES(@username, @password, @dateReg) ";
+
+                        using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@username", signup_username.Text.Trim());
+                            cmd.Parameters.AddWithValue("@password", signup_password.Text.Trim());
+                            cmd.Parameters.AddWithValue("@dateReg", today);
+
+                            cmd.ExecuteNonQuery();
+
+                            MessageBox.Show("Registered successfully!",
+                                "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            Form1 loginForm = new Form1();
+                            loginForm.Show();
+                            this.Hide();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex
+                         , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+
+        private void signup_showPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            //signup_password.UseSystemPasswordChar = !signup_showPassword.Checked;
+            signup_password.PasswordChar = signup_showPassword.Checked ? '\0' : '*';        }
+    }
+}
